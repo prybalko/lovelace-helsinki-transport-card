9030eb87ae2aef7ab702be2d6dc42d7bd99dabf5
Pavel Rybalko <paul.rybalko@gmail.com>
2025-07-20 16:02:41 +0300

    use Helsinki

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Lovelace card for Berlin (BVG) and Brandenburg (VBB) transport integration
+# Lovelace card for Helsinki (HSL) transport integration
 
-Custom lovelace card that displays upcoming departures from your defined public transport stops for Berlin and Brandenburg.
+Custom lovelace card that displays upcoming departures from your defined public transport stops for Helsinki.
 
-**The integration itself can be found here: https://github.com/vas3k/home-assistant-berlin-transport**
+**The integration itself can be found here: https://github.com/prybalko/home-assistant-helsinki-transport**
 
 This card works only after you installed and configured the integration.
 
@@ -14,33 +14,33 @@ This card works only after you installed and configured the integration.
 
 This Lovelace card can be installed via [HACS](https://hacs.xyz/) or manually.
 
-> ⚠️ Make sure you installed the [BVG integration](https://github.com/vas3k/home-assistant-berlin-transport) first. This card would not work without it.
+> ⚠️ Make sure you installed the [BVG integration](https://github.com/prybalko/home-assistant-helsinki-transport) first. This card would not work without it.
 
 ### Using HACS
 
 **1.** Open HACS interface from your Home Assistant sidebar
 
-**2.** Add [this repository](https://github.com/vas3k/lovelace-berlin-transport-card) as a custom repository (Three dots in top right corner -> Custom repositories)
+**2.** Add [this repository](https://github.com/prybalko/lovelace-helsinki-transport-card) as a custom repository (Three dots in top right corner -> Custom repositories)
 
 **3.** Select "Lovelace" as a category
 
-**4.** Go to `Settings -> Devices & Services -> Add integration` and search for this card name (just type `Berlin`)
+**4.** Go to `Settings -> Devices & Services -> Add integration` and search for this card name (just type `Helsinki`)
 
 **5.** Install it. Now you can add it to your dashboard
 
 
 ### Installing the card manually
 
-**1.** Copy the [berlin-transport-card.js](./dist) card module to the `www` directory of your Home Assistant. The same way you did for the sensor above. If it doesn't exist — create one.
+**1.** Copy the [helsinki-transport-card.js](./dist) card module to the `www` directory of your Home Assistant. The same way you did for the sensor above. If it doesn't exist — create one.
 
 **2.** Go to your Home Assistant dashboard, click "Edit dashboard" at the right top corner and after that in the same top right corner choose "Manage resources".
 
-**3.** Add new resource with URL: `/local/berlin-transport-card.js` and click create. Go back to your dashboard and refresh the page.
+**3.** Add new resource with URL: `/local/helsinki-transport-card.js` and click create. Go back to your dashboard and refresh the page.
 
 **4.** Now you can add the custom card and integrate it with your sensor. Click "Add card -> Manual" or just go to "Raw configuration editor" and use this config.
 
 ```yaml
-- type: custom:berlin-transport-card
+- type: custom:helsinki-transport-card
   show_stop_name: true # show or hide the name of your stop in card title
   max_entries: 8 # number of upcoming departures to show (max: 10)
   entities:
@@ -60,14 +60,11 @@ If you want to change any styles, font size or layout — the easiest way is to 
 
 ## ❤️ Contributions
 
-Contributions are welcome. Feel free to [open a PR](https://github.com/vas3k/lovelace-berlin-transport-card/pulls) and send it to review. If you are unsure, [open an Issue](https://github.com/vas3k/lovelace-berlin-transport-card/issues) and ask for advice.
+Contributions are welcome. Feel free to [open a PR](https://github.com/prybalko/lovelace-helsinki-transport-card/pulls) and send it to review. If you are unsure, [open an Issue](https://github.com/prybalko/lovelace-helsinki-transport-card/issues) and ask for advice.
 
 ## 🐛 Bug reports and feature requests
 
 Since this is my small hobby project, I cannot guarantee you a 100% support or any help with configuring your dashboards. I hope for your understanding.
-
-- **If you find a bug** - open [an Issue](https://github.com/vas3k/lovelace-berlin-transport-card/issues) and describe the exact steps to reproduce it. Attach screenshots, copy all logs and other details to help me find the problem.
-- **If you're missing a certain feature**, describe it in Issues and try to code it yourself. It's not hard. At the very least, you can try to [bribe me with a PayPal donation](https://www.paypal.com/paypalme/vas3kcom) to make the feature just for you :)
 
 ## 👮‍♀️ License
 
---
+++ dist/helsinki-transport-card.js
@@ -0,0 +1,173 @@
+// Helsinki Transport Card
+
+class HelsinkiTransportCard extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: 'open'
+        });
+    }
+
+    /* This is called every time sensor is updated */
+    set hass(hass) {
+
+        const config = this.config;
+        const maxEntries = config.max_entries || 10;
+        const showStopName = config.show_stop_name || (config.show_stop_name === undefined);
+        const entityIds = config.entity ? [config.entity] : config.entities || [];
+        const showCancelled = config.show_cancelled || (config.show_cancelled === undefined);
+        const showDelay = config.show_delay || (config.show_delay === undefined);
+        const showAbsoluteTime = config.show_absolute_time || (config.show_absolute_time === undefined);
+        const showRelativeTime = config.show_relative_time || (config.show_relative_time === undefined);
+        const includeWalkingTime = config.include_walking_time || (config.include_walking_time === undefined);
+
+        let content = "";
+
+        for (const entityId of entityIds) {
+            const entity = hass.states[entityId];
+            if (!entity) {
+                throw new Error("Entity State Unavailable");
+            }
+
+            if (showStopName) {
+                content += `<div class="stop">${entity.attributes.friendly_name}</div>`;
+            }
+
+            const timetable = entity.attributes.departures.slice(0, maxEntries).map((departure) => {
+                const delay = departure.delay === null ? `` : departure.delay / 60;
+                const delayDiv = delay > 0 ? `<div class="delay delay-pos">+${delay}</div>`: `<div class="delay delay-neg">${delay === 0 ? '+0' : delay}</div>`;
+                const currentDate = new Date().getTime();
+                const timestamp = new Date(departure.timestamp).getTime();
+                const walkingTime = includeWalkingTime ? departure.walking_time : 0;
+                const relativeTime = Math.round((timestamp - currentDate) / (1000 * 60)) - walkingTime;
+                const relativeTimeDiv = `<div class="relative-time">${relativeTime}&prime;&nbsp;</div>`;
+
+                return departure.cancelled && !showCancelled ? `` :
+                    `<div class="${departure.cancelled ? 'departure-cancelled' : 'departure'}">
+                    <div class="line">
+                        <div class="line-icon" style="background-color: ${departure.color}">${departure.line_name}</div>
+                    </div>
+                    <div class="direction">${departure.direction}</div>
+                    <div class="time">
+                        <span class="live" style="color: ${departure.is_live ? 'green' : 'gray'}">●</span>
+                        ${showRelativeTime ? relativeTimeDiv : ''}${showAbsoluteTime ? departure.time : ''}${showDelay ? delayDiv : ''}
+                    </div>
+
+                </div>`
+            });
+
+            content += `<div class="departures">` + timetable.join("\n") + `</div>`;
+        }
+
+        this.shadowRoot.getElementById('container').innerHTML = content;
+    }
+
+    /* This is called only when config is updated */
+    setConfig(config) {
+        const root = this.shadowRoot;
+        if (root.lastChild) root.removeChild(root.lastChild);
+
+        this.config = config;
+
+        const card = document.createElement('ha-card');
+        const content = document.createElement('div');
+        const style = document.createElement('style');
+
+        style.textContent = `
+            .container {
+                padding: 10px;
+                font-size: 130%;
+                line-height: 1.5em;
+            }
+            .stop {
+                opacity: 0.6;
+                font-weight: 400;
+                width: 100%;
+                text-align: left;
+                padding: 10px 10px 5px 5px;
+            }      
+            .departures {
+                width: 100%;
+                font-weight: 400;
+                line-height: 1.5em;
+                padding-bottom: 20px;
+            }
+            .departure {
+                padding-top: 10px;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                align-items: flex-start;
+                gap: 20px;
+            }
+            .departure-cancelled {
+                text-decoration: line-through;
+                filter: grayscale(50%);
+                padding-top: 10px;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                align-items: flex-start;
+                gap: 20px;
+            }
+            .line {
+                min-width: 70px;
+                text-align: right;
+            }
+            .line-icon {
+                display: inline-block;
+                border-radius: 20px;
+                padding: 7px 10px 5px;
+                font-size: 120%;
+                font-weight: 700;
+                line-height: 1em;
+                color: #FFFFFF;
+                text-align: center;
+            }
+            .direction {
+                align-self: center;
+                flex-grow: 1;
+            }
+            .time {
+                align-self: flex-start;
+                font-weight: 700;
+                line-height: 2em;
+                padding-right: 10px;
+                display: flex;
+            }
+            .delay {
+               line-height: 2em;
+               font-size: 70%;
+               text-align: right;
+               min-width: 2ch;
+            }
+            .delay-pos {
+               color: #8B0000;
+            }
+            .delay-neg {
+               color: #006400;
+            }
+            .relative-time {
+               font-style: italic;
+            }
+            .live {
+                margin-right: 5px;
+            }
+        `;
+
+        content.id = "container";
+        content.className = "container";
+        card.header = config.title;
+        card.appendChild(style);
+        card.appendChild(content);
+
+        root.appendChild(card);
+    }
+
+    // The height of the card.
+    getCardSize() {
+        return 5;
+    }
+}
+
+customElements.define('helsinki-transport-card', HelsinkiTransportCard);
